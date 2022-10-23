699fd400141b3daea72cc6b3bfbba8e69ad69837
Danzz Coding <muhammadramdani196453@gmail.com>
2022-10-23 18:24:59 +0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,25 @@ route.get('/', (req, res) => {
     res.sendFile(__path + '/index.html')
 });
 
-router.get('/api/shorturl', async (req, res, next) => {
+router.get('/api/shorturl/tinyuwithalias', async (req, res, next) => {
 	var url = req.query.url
 	var alias = req.query.alias
 	if (!url) return res.json({ message: 'Enter Params URL' })
 	if (!alias) return res.json({ message: 'Enter Params Alias (Name)' })
 	
 	let data = await fetchJson(`https://danzzapi.xyz/api/shortlink/tinyualias?url=${url}&alias=${alias}&apikey=danzz`)
+	res.json({
+	status: true,
+	author: 'Danzz Coding',
+	result: data.result
+	})
+})
+
+router.get('/api/shorturl/tinyunotwithalias', async (req, res, next) => {
+	var url = req.query.url
+	if (!url) return res.json({ message: 'Enter Params URL' })
+	
+	let data = await fetchJson(`https://danzzapi.xyz/api/shortlink/tinyurl?url=${url}&apikey=danzz`)
 	res.json({
 	status: true,
 	author: 'Danzz Coding',
